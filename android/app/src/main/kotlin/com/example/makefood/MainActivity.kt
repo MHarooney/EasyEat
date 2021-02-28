3fe550c687b86271b36ff2b72f7731494a6d0cf1
Mahmoud Al-Haroon <mahmoud.harooney.tooling@gmail.com>
2021-02-28 23:31:45 +0200

    Add Project

---
+++ android/app/src/main/kotlin/com/example/makefood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.makefood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:makefood/pizza_order/main_pizza_order.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MainPizzaOrderApp(),
+  ));
+}
